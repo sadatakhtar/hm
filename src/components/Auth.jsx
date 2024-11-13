41a8a37794fa9ce8e0050b2e0801bc293347b15e
sadat <sadat0akhtar@gmail.com>
2024-11-13 21:14:48 +0000

    Updated Signin screen --> Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,10 +1,23 @@
 // src/components/Auth.jsx
 import React, { useState } from "react";
-import { auth } from "../../firebase.config"
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../../firebase.config";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { useDispatch } from "react-redux";
 import { setUser, clearUser } from "../features/authenticationSlice";
 import { useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
@@ -14,10 +27,14 @@ const Auth = () => {
 
   const handleSignUp = async () => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const { uid, email: userEmail, displayName } = userCredential.user;
       dispatch(setUser({ uid, email: userEmail, displayName }));
-      console.log('first time sign up ...DONE!');
+      console.log("first time sign up ...DONE!");
     } catch (error) {
       console.error("Error signing up:", error);
     }
@@ -25,11 +42,15 @@ const Auth = () => {
 
   const handleSignIn = async () => {
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const { uid, email: userEmail, displayName } = userCredential.user;
       dispatch(setUser({ uid, email: userEmail, displayName }));
-      console.log('Signed IN!');
-        navigat('/home');
+      console.log("Signed IN!");
+      navigat("/home");
     } catch (error) {
       console.error("Error signing in:", error);
     }
@@ -39,32 +60,89 @@ const Auth = () => {
     try {
       await signOut(auth);
       dispatch(clearUser());
-      console.log('Signed out!!')
+      console.log("Signed out!!");
     } catch (error) {
       console.error("Error signing out:", error);
     }
   };
 
-  return (
-    <div>
-      <h1>Firebase Authentication</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSignIn}>Sign In</button>
-      <button onClick={handleSignUp}>Sign Up</button>
-      <button onClick={handleSignOut}>Sign Out</button>
-    </div>
+  const pageModel = (
+    <Container
+      component="main"
+      maxWidth="xs"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid grey",
+        marginLeft: 55,
+      }}
+    >
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h4" component="div" gutterBottom>
+          HomeMade Foods
+        </Typography>
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleSignIn} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            color="primary"
+            //autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            color="secondary"
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={handleSignIn}
+          >
+            Sign In
+          </Button>
+          <Button
+            fullWidth
+            variant="outlined"
+            sx={{ mt: 1, mb: 2 }}
+            onClick={handleSignUp}
+          >
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
+
+  return pageModel;
 };
 
 export default Auth;
